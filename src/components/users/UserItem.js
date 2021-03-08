6e3dbae471c7e4fef32304d929f9f9dc7101a82c
Emmanuel Perez <emmanueleperez>
2021-03-08 17:05:32 -0500

    first commit

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+class UserItem extends Component {
+    constructor() {
+        super();
+        this.state = {
+            id: 'id',
+            login: 'mojombo',
+            avatar_url: '',
+            html_url: ''
+        }
+    }
+    render() {
+        return (
+            <div className="card text-center">
+                <img 
+                    src={this.state.avatar_url} 
+                    alt="" 
+                    className="round-img" 
+                    style={{width: '60px'}}>
+                </img>
+                <h3>{this.state.login}</h3>
+                <div>
+                    <a href={this.state.html_url} className="btn btn-dark btn-sm my-1">More</a>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default UserItem
