ccf76acc8e5d6566d9741da764016d8c27716a7c
Emmanuel Perez <emmanueleperez>
2021-03-08 19:28:34 -0500

    implement search and spinner components

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,37 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
 import UserItem from './UserItem'
-class Users extends Component {
-    state = {
-        users: [
-            {
-                id: '1',
-                login: 'mojombo',
-                avatar_url: '',
-                html_url: ''
-            },
-            {
-                id: '2',
-                login: 'emmanuel',
-                avatar_url: '',
-                html_url: ''
-            },
-            {
-                id: '3',
-                login: 'bradtraversy',
-                avatar_url: '',
-                html_url: ''
-            }
-        ]
-    }
-    render() {
+import Spinner from '../layout/Spinner'
+import PropTypes from 'prop-types'
+
+const Users = ({ users, loading }) => {
+    if(loading) {
+        return <Spinner />
+    } else {
         return (
             <div style={userStyle}>
-               {this.state.users.map(user => (
-                   <UserItem key={user.id} user={user}/>
-               ))} 
+                {users.map(user => (
+                    <UserItem key={user.id} user={user}/>
+                ))} 
             </div>
         )
     }
+    
+}
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
 }
 const userStyle = {
     display: 'grid',
